0661dc9bbde68bdc557c253b1031d1cbb5023a1a
Akshay <akshaycm@hotmail.com>
2019-02-15 15:43:27 +0300

    chnage

--- core/src/main/java/org/cryptodezirecashj/core/Peer.java
+++ core/src/main/java/org/cryptodezirecashj/core/Peer.java
@@ -494,13 +494,13 @@ public class Peer extends PeerSocketHandler {
             currentFilteredBlock = null;
         }
 
-        // No further communication is possible until version handshake is complete.
-        if (!(m instanceof VersionMessage || m instanceof VersionAck
-                || (versionHandshakeFuture.isDone() && !versionHandshakeFuture.isCancelled()))) {
-            String reason = "  " + ((m instanceof RejectMessage) ? ((RejectMessage) m).getReasonString() : "");
-            throw new ProtocolException(
-                    "Received " + m.getClass().getSimpleName() + " before version handshake is complete."+ reason);
-        }
+        // // No further communication is possible until version handshake is complete.
+        // if (!(m instanceof VersionMessage || m instanceof VersionAck
+        //         || (versionHandshakeFuture.isDone() && !versionHandshakeFuture.isCancelled()))) {
+        //     String reason = "  " + ((m instanceof RejectMessage) ? ((RejectMessage) m).getReasonString() : "");
+        //     throw new ProtocolException(
+        //             "Received " + m.getClass().getSimpleName() + " before version handshake is complete."+ reason);
+        // }
 
         if (m instanceof Ping) {
             processPing((Ping) m);
